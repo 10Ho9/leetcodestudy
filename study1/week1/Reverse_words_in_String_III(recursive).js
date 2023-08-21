9fcfa0f420339ac987b56c7c3dbc84ad413c0dde
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-08-21 11:15:32 +0200

    Update Reverse_words_in_String_III(recursive).js

--- study1/week1/Reverse_words_in_String_III(recursive).js
+++ study1/week1/Reverse_words_in_String_III(recursive).js
@@ -2,6 +2,10 @@
  * @param {string} s
  * @return {string}
  */
+
+// 시간복잡도 : O(n) 주어진 문장의 단어의 갯수만큼 각각 한 번씩 reverseWord가 실행되니깐 시간복잡도는 O(n)
+// 공간복잡도 : O(n) reverseWord 함수에서 restCount가 0이 될 때까지 실행된 reverseWord가 스택에 쌓여 있기 때문에 공간복잡도는 O(n)
+
 function reverseWords(s) {
   const wordsArray = s.split(" ");
   const wordsCount = wordsArray.length;
