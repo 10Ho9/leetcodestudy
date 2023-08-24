0c9e0fe2e2f6737a92b52929cd75d2466d39dcba
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-08-24 22:16:55 +0200

    Create Set_Mismatch.js

---
+++ study1/week1/Set_Mismatch.js
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/set-mismatch/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+// 시간복잡도 : O(n) 주어진 nums의 크기 만큼 반복문이 실행 되어서 시간복잡도는 O(n)
+// 공간복잡도 : O(n) 주어진 nums의 크기 만큼 새로운 배열을 생성해서 공간복잡도는 O(n)
+
+function findErrorNums(nums) {
+  const correctSum = (nums.length * (nums.length + 1)) / 2;
+  const checkArray = new Array(nums.length);
+
+  let sum = 0;
+  let duplicate = 0;
+
+  for (num of nums) {
+    if (checkArray[num]) {
+      duplicate = num;
+      sum += num;
+    } else {
+      checkArray[num] = true;
+      sum += num;
+    }
+  }
+  return [duplicate, correctSum - sum + duplicate];
+};
