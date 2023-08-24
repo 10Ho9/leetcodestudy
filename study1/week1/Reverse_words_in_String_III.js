254061446ec1db56e35662d8ae7664be21dc2a28
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-08-24 20:41:08 +0200

    Update Reverse_words_in_String_III.js

--- study1/week1/Reverse_words_in_String_III.js
+++ study1/week1/Reverse_words_in_String_III.js
@@ -8,7 +8,7 @@
 // 시간복잡도 : O(n) 주어진 문장의 단어의 갯수만큼 각각 한 번씩 reverseWord가 실행되니깐 시간복잡도는 O(n)
 // 공간복잡도 : O(n) 주어진 문장에 따라 최대 s길이의 2배가 공간이 별도로 필요 공간복잡도는 O(n)
 
-function reverseWords (s) {
+function reverseWords(s) {
   const wordsArray = s.split(" ");
   
   for (let i = 0; i < wordsArray.lenght; i++) {
@@ -17,7 +17,7 @@ function reverseWords (s) {
   return wordsArray.join(" ");
 }
 
-function reverseWord (word) {
+function reverseWord(word) {
   word = word.split(""); 
   word.reverse();
   return word.join("")
