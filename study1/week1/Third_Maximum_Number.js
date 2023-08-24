c2ac2d074a72138a2c53c7da5f0d50a4b1cafc6f
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-08-24 20:41:28 +0200

    Update Third_Maximum_Number.js

--- study1/week1/Third_Maximum_Number.js
+++ study1/week1/Third_Maximum_Number.js
@@ -8,7 +8,7 @@
 //시간복잡도 : O(n) 주어진 배열의 크기 만큼 반복문을 실행하게 되므로 O(n)
 //공간복잡도 : O(1) 주어진 배열의 크기와 관계없이 항상 first, second, third의 공간만 사용하므로 O(1)
 
-function thirdMax (nums) {
+function thirdMax(nums) {
   let first = -2147483649;
   let second = -2147483649;
   let third = -2147483649;
