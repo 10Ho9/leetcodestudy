866b9d52d6011124bb911b0ec0c5a3e2a459eeb0
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-08-25 13:27:22 +0200

    Update Merge_Nodes_in_Between_Zeros.js

--- study1/week1/Merge_Nodes_in_Between_Zeros.js
+++ study1/week1/Merge_Nodes_in_Between_Zeros.js
@@ -13,21 +13,23 @@
  */
 
 // 시간복잡도 : O(n) 주어진 head의 길이 만큼 반복문이 실행 되어서 시간복잡도는 O(n)
-// 공간복잡도 : O(n) 주어진 head의 크기 n, 최대 ⌊(n-1)/2⌋ 크기의 새로운 node를 생성해서 공간복잡도는 O(n)
+// 공간복잡도 : O(1) 주어진 head를 사용해서 공간복잡도는 O(1)
 
 function mergeNodes(head) {
-  const result = new ListNode();
-  let temp = result;
+  let temp = head;
   let currNode = head.next;
 
   while (currNode) {
     if (currNode.val == 0 && currNode.next !== null) {
-      temp.next = new ListNode();
       temp = temp.next
+      temp.val = 0;
+    } else if (currNode.val == 0 && currNode.next == null) {
+      temp.next = null;
+      break;
     } else {
       temp.val += currNode.val;
     }
     currNode = currNode.next;
   }
-  return result;
+  return head;
 }
