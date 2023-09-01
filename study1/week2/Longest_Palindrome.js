68ae190ff81e9bc1cb801917fd50e91d10cc13f6
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-09-01 12:40:54 +0200

    Update Longest_Palindrome.js

--- study1/week2/Longest_Palindrome.js
+++ study1/week2/Longest_Palindrome.js
@@ -6,17 +6,16 @@
  */
 
 // 시간복잡도 : O(n) 주어진 s의 길이 만큼 반복문이 실행 되어서 시간복잡도는 O(n)
-// 공간복잡도 : O(1) 주어진 s와 관계없이 항상 length변수와 최대 59의 길이의 배열을 사용하기 때문에 공간복잡도는 O(1)
+// 공간복잡도 : O(1) 주어진 s와 관계없이 항상 최대 59의 길이의 배열을 사용하기 때문에 공간복잡도는 O(1)
 
 function longestPalindrome(s) {
-  s = new TextEncoder().encode(s);
-  let length = 0;
-  const lettersCountArray = [];
+  let result = 0;
+  const lettersCountArray = Array.from({length: 59}, () => 0);
 
-  for (const c of s) {
-    lettersCountArray[c - 65] = (lettersCountArray[c - 65] || 0) + 1;
-    if (lettersCountArray[c - 65] % 2 === 0) length += 2;
+  for (let i = 0; i <= s.length; i++) {
+    lettersCountArray[s.charCodeAt(i) - 65] += 1;
+    if (lettersCountArray[s.charCodeAt(i) - 65] % 2 === 0) result += 2;
   }
 
-  return s.length > length ? length + 1 : length;
+  return s.length > result ? result + 1 : result;
 }
