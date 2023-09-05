60763e1649c79d52d02666e3ac1099cf2def3e99
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-09-05 21:11:52 +0200

    Update Reduce_Array_Size_to_The_Half.js

--- study1/week3/Reduce_Array_Size_to_The_Half.js
+++ study1/week3/Reduce_Array_Size_to_The_Half.js
@@ -6,7 +6,7 @@
  */
 
 // 시간복잡도 : O(nlogn) 정렬을 사용해서 시간복잡도는 O(nlogn)
-// 공간복잡도 : O(n) 최대 주어진 root의 길이 만큼 배열이 사용 되어서 공간복잡도는 O(n)
+// 공간복잡도 : O(n) 최대 주어진 arr의 길이 만큼 배열이 사용 되어서 공간복잡도는 O(n)
 
 function minSetSize(arr) {
   let halfSize = arr.length / 2;
