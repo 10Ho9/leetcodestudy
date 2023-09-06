4d4b51e8e5f3988548f5074c7d635cf2fa93a1f2
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-09-06 11:54:12 +0200

    Update Reduce_Array_Size_to_The_Half.js

--- study1/week3/Reduce_Array_Size_to_The_Half.js
+++ study1/week3/Reduce_Array_Size_to_The_Half.js
@@ -6,24 +6,21 @@
  */
 
 // 시간복잡도 : O(nlogn) 정렬을 사용해서 시간복잡도는 O(nlogn)
-// 공간복잡도 : O(n) 최대 주어진 arr의 길이 만큼 배열이 사용 되어서 공간복잡도는 O(n)
+// 공간복잡도 : O(n) 최대 주어진 arr의 길이 만큼 배열 2개 사용 되어서 공간복잡도는 O(n)
 
 function minSetSize(arr) {
-  let halfSize = arr.length / 2;
-  const numsCounts = [];
+  const numsCounts = {};
 
   for (let i = 0; i < arr.length; i++) {
-    numsCounts[arr[i]] = numsCounts[arr[i]] + 1 || 1;
-    if (halfSize <= numsCounts[arr[i]]) return 1;
+    numsCounts[arr[i]] = numsCounts[arr[i]] === undefined ? 1 : numsCounts[arr[i]] + 1;
   }
-
-  arr = numsCounts.filter(((num) => num !== 0));
-  arr.sort((a,b) => b - a);
   
+  let sortedCounts = Object.values(numsCounts).sort((a,b) => b - a);
   let countRemoved = 0;
-
+  let halfSize = arr.length / 2;
+  
   while (0 < halfSize) {
-    halfSize -= arr[countRemoved];
+    halfSize -= sortedCounts[countRemoved];
     countRemoved++;
   }
 
